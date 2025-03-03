33ae0b87091f56ec99d81aea79a7cfa845858d01
DavezSant <103318146+davezant@users.noreply.github.com>
2025-03-03 20:24:42 -0300

    Refactoring main.go

--- main.go
+++ main.go
@@ -1,66 +1,74 @@
 package main
 
 import (
-	"fmt"
-	"os/exec"
-	"os"
-	"strings"
 	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 )
-/*Utils*/
+
+// Utility functions
 func removeTag(text, cutTerm string) string {
 	return strings.ReplaceAll(text, cutTerm, "")
 }
-func parseStringToLists(text, cutTerm string) []string{
+
+func parseStringToLists(text, cutTerm string) []string {
 	return strings.Split(text, cutTerm)
 }
-/*Network Checks*/
-func IsAliveWlan(verbose bool) bool{
-	cmd := exec.Command("bash","-c","iw wlan0 info")
-	output, err := cmd.Output()
-	if err!=nil {
-		fmt.Println("Does not exists?:", err)
-		return false
-			
-	}
-	
-	if verbose == true {fmt.Println(string(output), "\n")}
-	return true
 
+// Network checks
+func isAliveWlan(verbose bool) bool {
+	cmd := exec.Command("bash", "-c", "iw wlan0 info")
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Println("Does not exist:", err)
+		return false
+	}
+
+	if verbose {
+		fmt.Println(string(output), "\n")
+	}
+	return true
 }
 
-func IsActuallyConnected(verbose bool) string{
-	cmd := exec.Command("bash","-c","iw wlan0 info | grep 'ssid'")
+func isActuallyConnected(verbose bool) string {
+	cmd := exec.Command("bash", "-c", "iw wlan0 info | grep 'ssid'")
 	output, err := cmd.Output()
-	if err!=nil {
-			if verbose == true {fmt.Println("It's not connected at any network right now", "\n")}
-			return "nil"
+	if err != nil {
+		if verbose {
+			fmt.Println("It's not connected to any network right now", "\n")
+		}
+		return "nil"
 	}
 	result := removeTag(string(output), "\tssid")
-	if verbose == true{fmt.Println("Is Actually Connected to: " + result, "\n")}
+	if verbose {
+		fmt.Println("Is actually connected to:", result, "\n")
+	}
 	return result
-	
 }
-func NetworkScanIntoArray(verbose bool) []string{
-	cmd := exec.Command("bash","-c","iw wlan0 scan | grep 'SSID:'")
+
+func networkScanIntoArray(verbose bool) []string {
+	cmd := exec.Command("bash", "-c", "iw wlan0 scan | grep 'SSID:'")
 	output, err := cmd.Output()
-	if err!=nil {
-		if verbose == true{
+	if err != nil {
+		if verbose {
 			fmt.Println("Error while scanning")
 		}
 		return []string{}
 	}
-	
+
 	result := removeTag(string(output), "\tSSID:")
-	if verbose == true{
-		print(string(result))
+	if verbose {
+		fmt.Print(string(result))
 	}
 
 	return parseStringToLists(result, "\n")
 }
-/*Set Auth*/
-func GetConfigurationSimple(SSID, PWD string) string{
-	cmd := exec.Command("bash","-c","wpa_passphrase " + SSID +" " + PWD)
+
+// Set authentication
+func getConfigurationSimple(SSID, PWD string) string {
+	cmd := exec.Command("bash", "-c", "wpa_passphrase "+SSID+" "+PWD)
 	output, err := cmd.Output()
 	if err != nil {
 		fmt.Println("Error")
@@ -69,34 +77,40 @@ func GetConfigurationSimple(SSID, PWD string) string{
 	fmt.Println(string(output))
 	return string(output)
 }
-/*Configuration File*/
-func WriteConfigFile(config string){
-	f, err := os.Create("/etc/wpa_supplicant/wpa_supplicant.conf")
-	if err!=nil {
-		fmt.Println("ERROR")
-		return	
-	}
-	l, err := f.WriteString(config)
-	fmt.Println(l, "bytes written sucessfully")
-	err = f.Close()
-	}
 
-func ReadConfigFile(){
-	f, err := os.Open("/etc/wpa_supplicant/wpa_supplicant.conf")
-	if err!=nil {
+// Configuration file operations
+func writeConfigFile(config string) {
+	f, err := os.Create("/etc/wpa_supplicant/wpa_supplicant.conf")
+	if err != nil {
 		fmt.Println("ERROR")
-		return	
+		return
 	}
 	defer f.Close()
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan(){
-		fmt.Println(scanner.Text())
-		
+
+	l, err := f.WriteString(config)
+	if err != nil {
+		fmt.Println("ERROR")
+		return
 	}
-	
+	fmt.Println(l, "bytes written successfully")
 }
-/*Terminal Interaction*/
-func OpenNetworkOption(netArray []string){
+
+func readConfigFile() {
+	f, err := os.Open("/etc/wpa_supplicant/wpa_supplicant.conf")
+	if err != nil {
+		fmt.Println("ERROR")
+		return
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
+// Terminal interaction
+func openNetworkOption(netArray []string) {
 	var i int
 	var pwd string
 	for index, ssid := range netArray {
@@ -106,15 +120,9 @@ func OpenNetworkOption(netArray []string){
 	SSIDnew := netArray[i]
 	fmt.Println("PASSWORD:")
 	fmt.Scan(&pwd)
-	WriteConfigFile(GetConfigurationSimple(SSIDnew, pwd))
+	writeConfigFile(getConfigurationSimple(SSIDnew, pwd))
 }
-func main(){
-/*
-	fmt.Println(IsAliveWlan(false))
-	fmt.Println(IsActuallyConnected(false))
-	fmt.Println(NetworkScanIntoArray(false))
-	s := GetConfigurationSimple("LIMA_2.4G","878915@@")
-	WriteConfigFile(s)
-*/
-	OpenNetworkOption(NetworkScanIntoArray(false))
+
+func main() {
+	openNetworkOption(networkScanIntoArray(false))
 }
